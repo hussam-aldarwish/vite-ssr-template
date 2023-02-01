49439737a948e72c367c30088d26f227a9551a12
Hussam Aldarwish <hussam.aldarwish@hotmail.com>
2023-02-01 09:58:07 +0300

    🚀 Optimize the build process and SSR options

--- src/client/entry-client.jsx
+++ src/client/entry-client.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const container = document.getElementById('root');
-
-const FullApp = () => (
+hydrateRoot(
+  document.getElementById('root'),
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-if (import.meta.hot || !container?.innerText) {
-  const root = createRoot(container);
-  root.render(<FullApp />);
-} else {
-  hydrateRoot(container, <FullApp />);
-}
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,28 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist/client',
-  },
+export default defineConfig(({ ssrBuild }) => {
+  return ssrBuild
+    ? {
+        plugins: [react()],
+        build: {
+          outDir: 'dist/server',
+          ssr: true,
+          ssrManifest: true,
+          copyPublicDir: false,
+          rollupOptions: {
+            input: 'src/client/entry-server.jsx',
+          },
+        },
+        ssr: {
+          noExternal: true,
+          external: ['react', 'react-dom'],
+        },
+      }
+    : {
+        plugins: [react()],
+        build: {
+          outDir: 'dist/client',
+        },
+      };
 });
