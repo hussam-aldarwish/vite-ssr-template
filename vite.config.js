ff68e619d5f826f15d5095c2e670fa9401fbb5c2
Hussam Aldarwish <hussam.aldarwish@hotmail.com>
2023-02-02 05:27:26 +0300

    🔃 chore: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const commonConfig = { plugins: [react()] };
+
+const ssrConfig = {
+  build: {
+    outDir: 'dist/client-ssr',
+    ssr: true,
+    ssrManifest: true,
+    copyPublicDir: false,
+    rollupOptions: {
+      input: 'src/client/entry-server.jsx',
+    },
+  },
+  ssr: {
+    noExternal: true,
+    external: ['react', 'react-dom'],
+  },
+};
+
+const clientConfig = {
+  build: {
+    outDir: 'dist/client',
+  },
+};
+
 // https://vitejs.dev/config/
-export default defineConfig(({ ssrBuild }) => {
-  return ssrBuild
-    ? {
-        plugins: [react()],
-        build: {
-          outDir: 'dist/client-ssr',
-          ssr: true,
-          ssrManifest: true,
-          copyPublicDir: false,
-          rollupOptions: {
-            input: 'src/client/entry-server.jsx',
-          },
-        },
-        ssr: {
-          noExternal: true,
-          external: ['react', 'react-dom'],
-        },
-      }
-    : {
-        plugins: [react()],
-        build: {
-          outDir: 'dist/client',
-        },
-      };
-});
+export default defineConfig(({ ssrBuild }) => ({
+  ...commonConfig,
+  ...(ssrBuild ? ssrConfig : clientConfig),
+}));
